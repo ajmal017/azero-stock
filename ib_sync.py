33c1e3f4648e34766575cb5527ac07e24eacfb50
Xin Huang <hxssg1124@gmail.com>
2018-08-13 22:41:49 +0800

    commit

--- ib_sync.py
+++ ib_sync.py
@@ -6,6 +6,8 @@ from dateutil import relativedelta
 from ib_api import *
 from utils import *
 
+logger = setup_logger('ib_sync_log', 'ib_sync.log')
+
 
 def get_earliest_time(app, contract):
     earliest_time = '%s 00:00:00' % app.req_head_time_stamp(500, contract)[0][1].split()[0]
@@ -56,8 +58,8 @@ def sync_stock(app, contract):
         if not hist_data:
             return
 
-        print('%s start syncing %s, %s-%s...' % (datetime.datetime.today().strftime("%Y%m%d %H:%M:%S"),
-                                                 symbol, hist_data[0][2].date, hist_data[-2][2].date))
+        logger.info('%s start syncing %s, %s-%s...' % (datetime.datetime.today().strftime("%Y%m%d %H:%M:%S"),
+                                                       symbol, hist_data[0][2].date, hist_data[-2][2].date))
         f_map = {}
         for data in hist_data:
             if data[1] == 'historical_data':
@@ -66,22 +68,23 @@ def sync_stock(app, contract):
                 if '%s_%s' % (symbol, sd) not in f_map:
                     f_map['%s_%s' % (symbol, sd)] = open('ib_data/%s_%s_1M.log' % (symbol, sd), 'w')
                 f = f_map['%s_%s' % (symbol, sd)]
-                f.writelines('%s~%s~%s~%s~%s~%s' % (bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume))
+                f.writelines('%s~%s~%s~%s~%s~%s\n' % (bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume))
             elif data[1] == 'historical_data_end':
                 dt = data[2]
         for f in f_map.values():
             f.close()
-        print('%s syncing %s, %s-%s done' % (datetime.datetime.today().strftime("%Y%m%d %H:%M:%S"),
-                                             symbol, hist_data[0][2].date, hist_data[-2][2].date))
+            logger.info('%s syncing %s, %s-%s done' % (datetime.datetime.today().strftime("%Y%m%d %H:%M:%S"),
+                                                       symbol, hist_data[0][2].date, hist_data[-2][2].date))
 
         client_id += 1
 
 
 def main():
     contracts = read_stock_contracts()
-    app = IBApp("localhost", 4001, 10)
+    app = IBApp("localhost", 4001, 20)
+    logger.info('start syncing....')
 
-    for i, contract in enumerate(contracts):
+    for i, contract in enumerate(contracts[200:]):
         if i == 2:
             break
         sync_stock(app, make_contract(contract.symbol, 'SMART'))
